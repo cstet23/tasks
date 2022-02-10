467ea007c49478f47a8294f4adf3dc9a649b6cdf
Colin Stetler <cstetler@udel.edu>
2022-02-09 19:55:40 -0500

    Added Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App(): JSX.Element {
                 Colin Stetler from CISC275 will be epic in 4.3 seconds.
             </header>
             <p>You cannot stop this. You are powerless.</p>
+            <p>Hello World</p>
         </div>
     );
 }
