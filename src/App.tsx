051b0cb35247724035fd674b5e4335a94af201b1
Colin Stetler <cstetler@udel.edu>
2022-03-18 17:03:07 -0400

    Added sketch and (empty) completed requirements list

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,8 @@ import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion
 import { ChangeColor } from "./form-components/ChangeColor";
 import { Quizzer } from "./quizzer/Quizzer";
 import { ShowOthers } from "./quizzer/ShowOthers";
+import sketch1 from "./assets/quizzersketchp1.jpg";
+import sketch2 from "./assets/quizzersketchp2.jpg";
 
 function App(): JSX.Element {
     const [visible, setVisible] = useState<boolean>(true);
@@ -68,6 +70,23 @@ function App(): JSX.Element {
                     <CycleHoliday></CycleHoliday>
                 </div>
             )}
+            <div>
+                <h3>Sketches</h3>
+                <img
+                    src={sketch1}
+                    alt="Quizzer sketch, main view and opened quiz view"
+                />
+            </div>
+            <div>
+                <img src={sketch2} alt="Quizzer sketch, edit quiz view" />
+            </div>
+            <hr></hr>
+            <div>
+                <h3>Completed Requirements:</h3>
+                <ul>
+                    <li>none yet :(</li>
+                </ul>
+            </div>
         </div>
     );
 }
